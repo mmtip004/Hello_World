4d583ca64d7d86d9625d0649101fd89928551711
mmtip004 <49160816+mmtip004@users.noreply.github.com>
2019-08-08 13:27:04 -0700

    Add files via upload

---
+++ Hello_World/First_Class.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class First_Class here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class First_Class
+{
+    private int x;
+
+    public First_Class()
+    {
+        x = 0;
+    }
+
+    public int sampleMethod(int y)
+    {
+        return x + y;
+    }
+}
